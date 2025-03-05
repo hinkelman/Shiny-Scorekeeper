6bbbd171a2ea5907bbed3b34ec0dbe82332dff33
Travis Hinkelman <thinkelman@pm.me>
2025-03-04 22:18:52 -0800

    Split roster into UI and Server

--- R/rosterServer.R
+++ R/rosterServer.R
@@ -1,35 +1,4 @@
 
-# Sidebar -----------------------------------------------------------------
-
-rosterSB <- function(id){
-  ns <- NS(id)
-  tagList(
-    actionButton(ns("save_teams_roster_changes"), "Save changes", icon = icon("save"), disabled = TRUE),
-    actionButton(ns("set_roster"), "Set roster", icon = icon("edit"), disabled = TRUE)
-  )
-}
-
-# UI ----------------------------------------------------------------------
-
-rosterUI <- function(id){
-  ns <- NS(id)
-  tagList(
-    DTOutput(ns("teamsTable")),
-    br(),
-    layout_column_wrap(
-      actionButton(ns("add_teams_row"), "Add row", icon = icon("plus-square")),
-      actionButton(ns("delete_teams_row"), "Delete row", icon = icon("trash"), disabled = TRUE)
-    ),
-    hr(),
-    DTOutput(ns("rosterView")),
-    br(),
-    uiOutput(ns("rosterAddDelete")),
-    uiOutput(ns("previousPlayers"))
-  )
-}
-
-# Server ------------------------------------------------------------------
-
 rosterServer <- function(id){
   moduleServer(id, function(input, output, session) {
     ns = NS(id)
@@ -223,5 +192,3 @@ rosterServer <- function(id){
     reactive(if (input$set_roster == 0) NULL else rv[["roster"]])
   })
 }
-
-
